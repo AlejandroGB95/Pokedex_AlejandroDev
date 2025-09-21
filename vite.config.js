d56ea236b33b50873d45b6e96b710cf91c3dd202
Alejandro <alexdack95@gmail.com>
2025-09-22 00:04:38 +0200

    WIP - cambios locales antes de revisar gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Pokedex_AlejandroDev/', // 👈 cambia esto al nombre real del repo
+  base: '/Pokedex_AlejandroDev/', // 👈 debe ser el nombre del repo en GitHub
 })
